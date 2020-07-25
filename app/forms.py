9dcff8419fe56894df7c9794da2b5b91795684b1
Marcus Ackland <mackland@kth.se>
2020-07-25 21:36:47 +0100

    add followers/following

--- app/forms.py
+++ app/forms.py
@@ -44,3 +44,7 @@ class EditProfileForm(FlaskForm):
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
                 raise ValidationError('Username already taken.')
+
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
