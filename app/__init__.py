86ae63bba5fd27132a77a39592716778078cf59d
Marcus Ackland <mackland@kth.se>
2020-07-30 21:24:23 +0100

    Use flask-bootstrap to improve look of app

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 from logging.handlers import RotatingFileHandler
 from config import Config
 
@@ -13,6 +14,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
