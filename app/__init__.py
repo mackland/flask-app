c775fa973629b0fccbf0c594f8629cb9ead4813e
Marcus Ackland <mackland@kth.se>
2020-08-01 16:09:40 +0100

    refactor to make app more modular with Blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -18,17 +18,20 @@ login.login_view = 'login'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-from app import routes, models, errors
+from app.errors import bp as errors_bp
+app.register_blueprint(errors_bp)
+
+from app import routes, models
 
 if not app.debug:
-	if not os.path.exists('logs'):
-		os.mkdir('logs')
-	file_handler = RotatingFileHandler('logs/flask-app.log', maxBytes=10240, backupCount=10)
-	file_handler.setFormatter(logging.Formatter(
-		'%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
-	))
-	file_handler.setLevel(logging.INFO)
-	app.logger.addHandler(file_handler)
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/flask-app.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    ))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
 
-	app.logger.setLevel(logging.INFO)
-	app.logger.info('Flask-app startup')
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Flask-app startup')
